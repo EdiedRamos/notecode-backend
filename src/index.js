840e27415fc3e7c9638ba51562524e86a9abe01f
EdiedRamos <edieisa22@gmail.com>
2024-06-03 03:37:03 -0500

    feat: configure cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const bodyParse = require("body-parser");
 const express = require("express");
+const cors = require("cors");
 
 const { DEFAULT_PORT } = require("./config");
 const routerApi = require("./routes");
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT ?? DEFAULT_PORT;
 
 app.use(bodyParse.json());
+app.use(cors());
 routerApi(app);
 
 app.listen(PORT, () => console.log(`Ready on ${PORT}`));
